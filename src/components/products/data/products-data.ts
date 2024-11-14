78d3088671695080f991db0a46dd4e99aab0530a
ian eddie <ianeddie2@gmail.com>
2024-11-14 00:28:24 -0300

    first commit

---
+++ src/components/products/data/products-data.ts
@@ -0,0 +1,59 @@
+import blackJacket from '../assets/black-jacket.webp';
+import blackJacketFront from '../assets/black-jacket-front.webp';
+import blackJacketBack from '../assets/black-jacket-back.webp';
+import redJacket from '../assets/red-jacket.webp';
+import redJacketFront from '../assets/red-jacket-front.webp';
+import redJacketBack from '../assets/red-jacket-back.webp';
+import blueJacket from '../assets/blue-jacket.webp';
+import blueJacketFront from '../assets/blue-jacket-front.webp';
+import blueJacketBack from '../assets/blue-jacket-back.webp';
+import grayJacket from '../assets/gray-jacket.webp';
+import grayJacketFront from '../assets/gray-jacket-front.webp';
+import grayJacketBack from '../assets/gray-jacket-back.webp';
+
+export const PRODUCTS = [
+  {
+    id: '01',
+    name: 'Black Jacket',
+    slug: 'black-jacket',
+    description: 'description',
+    image: blackJacket,
+    imageFront: blackJacketFront,
+    imageBack: blackJacketBack,
+    quantity: 10,
+    price: 100
+  },
+  {
+    id: '02',
+    name: 'Blue Jacket',
+    slug: 'blue-jacket',
+    description: 'description',
+    image: blueJacket,
+    imageFront: blueJacketFront,
+    imageBack: blueJacketBack,
+    quantity: 10,
+    price: 130
+  },
+  {
+    id: '03',
+    name: 'Red Jacket',
+    slug: 'red-jacket',
+    description: 'description',
+    image: redJacket,
+    imageFront: redJacketFront,
+    imageBack: redJacketBack,
+    quantity: 10,
+    price: 90
+  },
+  {
+    id: '04',
+    name: 'Gray Jacket',
+    slug: 'gray-jacket',
+    description: 'description',
+    image: grayJacket,
+    imageFront: grayJacketFront,
+    imageBack: grayJacketBack,
+    quantity: 10,
+    price: 120
+  }
+];
---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      height: {
+        screen: '100dvh'
+      },
+      boxShadow: {
+        custom: '7px 7px 3px rgba(12, 10, 9, 0.5)'
+      },
+      fontFamily: {
+        primary: ['ClashDisplay-Variable']
+      }
+    }
+  },
+  plugins: []
+};
